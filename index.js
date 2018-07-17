ae78ec7b1f47362314a078fb688400e3341dcfd4
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-17 20:01:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 var currentUser= "currentUser";
 var welcomeMessage="Welcome to Flatbook, " + currentUser + "!";
-var excitedWelcomeMessage="WELCOME TO FLATBOOK, ";
+var excitedWelcomeMessage="WELCOME TO FLATBOOK, " + currentUser + "!";
 const upperCaseCurrentUser = currentUser.toUpperCase(excitedWelcomeMessage);
 
 var shortGreeting="Welcome, "+"!";
