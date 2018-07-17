487e9af38264ff9772260a28f2e1ad90db255333
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-17 20:03:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ var currentUser= "currentUser";
 var welcomeMessage="Welcome to Flatbook, " + currentUser + "!";
 const upperCaseCurrentUser = currentUser.toUpperCase();
 var excitedWelcomeMessage="WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!";
-
-
 var shortGreeting="Welcome, "+"!";
