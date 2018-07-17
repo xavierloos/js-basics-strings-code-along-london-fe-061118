e80b87afce431f60d05891a9351881fced47bc0e
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-17 20:03:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+var currentUser= "currentUser";
+var welcomeMessage="Welcome to Flatbook, " + currentUser + "!";
+const upperCaseCurrentUser = currentUser.toUpperCase();
+var excitedWelcomeMessage="WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!";
+var shortGreeting="Welcome, "+"!";
